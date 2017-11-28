61324f59871f87824f0f004dfef9694fdb37faa1
TonyC <ml1453724@gmail.com>
2017-11-28 15:43:46 -0500

    S

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
 var app = express();
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/index.html" );
+	res.redirect('/home');
+});
 
+app.get('/home', function (req, res) {
+	//es.send('home page');
+   	res.sendFile( __dirname + "/home.html" );
 })
 
-app.post('/', urlencodedParser, function (req, res) {
-	var obj = {};
-	console.log(req.body);
-	res.send(req.body);
-});
+// Get buiding list
+app.get('/list_building', function (req, res) {
+	console.log("list_buiding GET request")
+	res.send('buiding list page');
+    //res.sendFile( __dirname + "/list.html" );
+})
+
+// Get reviews
+app.get('/reviews', function (req, res) {
+	console.log("reviews GET request")
+	res.send('reviews page');
+    //res.sendFile( __dirname + "/reviews.html" );
+})
+
+
+// app.post('/', urlencodedParser, function (req, res) {
+// 	var obj = {};
+// 	console.log(req.body);
+// 	res.send(req.body);
+// });
 
 var server = app.listen(3000, function () {
  
   var host = server.address().address
   var port = server.address().port
  
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+  console.log(`Server listening on port ${port}`);
  
 })

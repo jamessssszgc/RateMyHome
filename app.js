3da78fc8a496680e0011445dce28b562c5f3c16a
jamessssszgc <zhangg18@outlook.com>
2017-11-28 22:43:03 -0500

    express

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+const url = "mongodb://manager:123@ds123896.mlab.com:23896/melonskin"
+app.use(bodyParser.urlencoded({extended: true}));
+
+ 
+app.use(express.static(__dirname +'/public'));
+const PORT = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+var db
+
+MongoClient.connect(url, function(err,res){
+	if(err) console.log(err)
+	console.log("Database created");
+	db = res
+});
+
+
+app.get('/', function (req, res) {
+	res.redirect('/home');
+});
+
+app.get('/home', function (req, res) {
+	//find all reviews which are not empty string
+   	db.collection('REVIEWS').find({"reviews":{$exists:true, $ne:""}}).toArray((err, result) => {
+    	if (err) return console.log(err)
+    	// renders index.ejs
+    	res.render('index', {REVIEWS:result})
+  	})
+   
+   //count the number rate = 1
+   //count the number rate = 2
+   //count the number rate = 3
+   //count the number rate = 4
+   //count the number rate = 5
+})
+
+
+// // Get buiding list
+// app.get('/list_building', function (req, res) {
+// 	console.log("list_buiding GET request")
+// 	res.send('buiding list page');
+//     //res.sendFile( __dirname + "/list.html" );
+// })
+
+// // Get reviews
+// app.get('/reviews', function (req, res) {
+// 	console.log("reviews GET request")
+// 	res.send('reviews page');
+//     //res.sendFile( __dirname + "/reviews.html" );
+// })
+
+
+
+app.post('/comment',(req, res) => {
+	
+	db.collection("REVIEWS").insertOne(req.body, function(err, result){
+					//continue
+					if (err) return console.log(err)
+					console.log("Database inserted");
+					res.redirect('/')
+				});
+
+	
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+
+
+

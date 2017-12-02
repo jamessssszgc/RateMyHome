0aafd92509d641f99023ffc3b7479fac6c35c35a
TonyC <ml1453724@gmail.com>
2017-12-01 20:56:28 -0500

    s

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,6 @@ router.get('/register', function(req, res) {
 
 router.post('/register', function(req, res) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-
         if (err) {
             return res.render('register', { account : account });
         }
